d75c390db25fff9f3d707ebd1d4c07d572e4576e
Michael Newman <coloradohokie@gmail.com>
2020-01-16 13:23:34 -0700

    initial commit

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 4, 'Male', 'Brown', 'chill', 'true');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 4, 'Male', 'Brown', 'bitchy', 'true');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 6, 'Female', 'Brown', 'happy', 'true');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 2, 'Male', 'Brown', 'serious', 'false');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 2, 'Female', 'Red', 'irritable', 'true');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 8, 'Male', 'Black', 'hangry', 'true');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 5, 'Female', 'Red', 'chill', 'false');
+INSERT INTO bears (age, gender, color, temperament, alive) VALUES (4, 'Male', 'Black', 'evil', 'true');
